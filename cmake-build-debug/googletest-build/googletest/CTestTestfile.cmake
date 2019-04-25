bf7d64daeee78bde0a78cf3e6b1658b6c6391c7b
RobinC94 <sissure@qq.com>
2019-04-25 11:05:33 +0800

    delete

--- cmake-build-debug/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/robin.chen/ClionProjects/2048/cmake-build-debug/googletest-src/googletest
-# Build directory: /Users/robin.chen/ClionProjects/2048/cmake-build-debug/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
